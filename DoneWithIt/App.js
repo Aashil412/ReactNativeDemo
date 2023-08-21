55054e9c1c5417183ad3580e78479762d28e0046
Aashi <aashilahmad@gmail.com>
2023-08-21 18:31:54 -0400

    first commit

---
+++ DoneWithIt/App.js
@@ -0,0 +1,36 @@
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  SafeAreaView,
+  Alert,
+  Image,
+  Button,
+  StatusBar,
+  Platform,
+  Dimensions,
+} from "react-native";
+import {useDeviceOrientation} from '@react-native-community/hooks'
+
+export default function App() {
+  const { landscape } = useDeviceOrientation();
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: 'dodgerblue', width: '100%', height: landscape ? '100%' : '30%'}}>
+
+      </View>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ?  StatusBar.currentHeight : 0,
+  },
+});
